9bfdd9a0e492ab5fe33358ac2f821beb6c6c1e41
SomyaSikarwar <118437254+SomyaSikarwar@users.noreply.github.com>
2023-11-05 12:04:11 +0530

    modified dknapsack.cpp

--- dknapsack.cpp
+++ dknapsack.cpp
@@ -20,6 +20,18 @@ int knapsack(int capacity, int weight[], int profit[], int n) {
         }
     }
 
+    // Backtrack to find selected items
+    int x[n] = {0}; // Initialize an array to track selected items
+    i = n;
+    w = capacity;
+    while (i > 0 && w > 0) {
+        if (Knapsack[i][w] != Knapsack[i - 1][w]) {
+            x[i - 1] = 1;
+            w -= weight[i - 1];
+        }
+        i--;
+    }
+
     cout << "\nMatrix:\n";
     for (i = 0; i <= n; i++) {
         for (w = 0; w <= capacity; w++) {
@@ -27,10 +39,17 @@ int knapsack(int capacity, int weight[], int profit[], int n) {
         }
         cout << endl;
     }
-    
-     return Knapsack[n][capacity];
 
+    cout << "\nSelected items:\n";
+    for (i = 0; i < n; i++) {
+        if (x[i] == 1) {
+            cout << "Item " << i + 1 << " (Profit: " << profit[i] << ", Weight: " << weight[i] << ")\n";
+        }
+    }
+
+    return Knapsack[n][capacity];
 }
+
 int main() {
     int n;
     cout << "Enter the number of items: ";
@@ -40,13 +59,11 @@ int main() {
     cin >> capacity;
     int weight[n], profit[n];
     for (int i = 0; i < n; i++) {
-        cout << "Enter profit of item" << i + 1 << ": ";
+        cout << "Enter profit of item " << i + 1 << ": ";
         cin >> profit[i];
-        cout << "Enter weight of item" << i + 1 << ": ";
+        cout << "Enter weight of item " << i + 1 << ": ";
         cin >> weight[i];
     }
     cout << "\nValue: " << knapsack(capacity, weight, profit, n) << endl;
 
-
-   
 }
